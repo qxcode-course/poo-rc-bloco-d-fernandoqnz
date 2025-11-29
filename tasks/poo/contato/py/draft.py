92708476512ca4e5a3c4ec278ec9005a3cc3e2de
Fernando Castro <fernand4603b@gmail.com>
2025-11-29 15:02:17 +0000

    finalizei a do contato

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -21,24 +21,95 @@ class Phone:
         return f"{self.__id}:{self.__number}"
 
 class Contact:
-    def __init__(self, name=""):
-        if name is None: name = ""
-        self.__name = name
-        self.__fones = []
-        self.__favorited = False
+    def __init__(self,name:str):
+        self.__name= name
+        self.__phones: list[Phone] = []
+        self.__favorited=False
+
+    def getName(self):
+        return self.__name
     
-    def addPhone(self, label, number):
-        fone = Phone(label, number)
-        if not fone.isValid(number):
-          print("fail: fone invalido")
-            return False
-        self.__fones.append(fone)
-        return True
+    def getIsfavorited(self):
+        return self.__favorited
+    
+    def getPhones(self):
+        return self.__phones
+    
+    def favorite(self):
+        self.__favorited=True
+
+    def unfavorited(self):
+        self.__favorited=False
+
+    def  addPhone(self, phone:Phone):
+        if phone.isValid(phone.getNumber()):
+            self.__phones.append(phone)
+            return True
+        print("fail: invalid number")
+        return False
+        
+
+    def rmPhone(self, index:int):
+        if 0 <= index <len(self.__phones):
+            del self.__phones[index]
+            return True
+        print("fail: telefone inexistente")
+        return False 
+
+    def __str__(self):
+        fav = "@" if self.__favorited else "-"
+
+        if len(self.__phones) == 0:
+            phones_str="[]"
+            return f"{fav} {self.__name} {phones_str}"
+        
+        phones_inside = ", ".join(f"{p.getId()}:{p.getNumber()}" for p in self.__phones)
+        return f"{fav} {self.__name} [{phones_inside}]"
+    
+def main():
+    contact =Contact("")
+    while True:
+        line=input()
+        args=line.split(" ")
+        print(f"${' '.join(args)}")
+        cmd = args[0]
+
+        if cmd == "show": 
+            print(contact)
+
+        elif cmd=="end":
+            break
+
+        elif cmd =="init":
+            contact = Contact(args[1])
+
+        elif cmd == "add":
+            id= args[1]
+            number= args [2]
+            contact.addPhone(Phone(id, number))
+
+        elif cmd == "rm":
+            index = int(args[1])
+            contact.rmPhone(index)
+
+        elif cmd == "tfav":
+            if contact.getIsfavorited():
+                contact.unfavorited()
+            else:
+                contact.favorite()
+
+        else: 
+            print("comando invdo")
+
+main()
+
+
+
+
 
         
 
-
-        
+    
 
         
 
